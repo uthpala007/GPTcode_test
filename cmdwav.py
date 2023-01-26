7686a650c34914aa0f05ce8a9407852c5d236f95
uthpala007 <72407165+uthpala007@users.noreply.github.com>
2023-01-26 12:48:07 +0200

    Removed the extra code

--- cmdwav.py
+++ cmdwav.py
@@ -67,38 +67,3 @@ wav_file.writeframes(sine_wave_with_break)
 wav_file.close()
 
 print("Wav file has been created and saved in the current directory with name")
-
-
-""" import argparse
-import numpy as np
-import wave
-
-def generate_tone(frequency, amplitude, duration, sample_rate, vibrato_rate=5, vibrato_depth=0.1):
-    num_frames = int(sample_rate * duration)
-    time_array = np.linspace(0, duration, num_frames)
-    modulation_wave = np.sin(2 * np.pi * vibrato_rate * time_array)
-    frequency_modulation = modulation_wave * vibrato_depth
-    modulated_frequency = frequency + frequency_modulation
-    samples = amplitude * np.sin(2 * np.pi * modulated_frequency * np.arange(num_frames) / sample_rate)
-
-    return samples
-
-def save_to_wav(samples, sample_rate, filename):
-    wav_file = wave.open(filename, "w")
-    wav_file.setparams((1, 2, sample_rate, 0, "NONE", "not compressed"))
-    wav_file.writeframes(samples.tostring())
-    wav_file.close()
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--frequency", type=float, help="frequency of the tone in hertz", required=True)
-parser.add_argument("-a", "--amplitude", type=float, help="amplitude of the tone as a percentage", required=True)
-parser.add_argument("-d", "--duration", type=float, help="duration of the tone in seconds", required=True)
-parser.add_argument("-vr", "--vibrato_rate", type=float, help="vibrato rate of the tone in Hz", default=5)
-parser.add_argument("-vd", "--vibrato_depth", type=float, help="vibrato depth of the tone in percentage", default=0.1)
-
-
-args = parser.parse_args()
-sample_rate = 44100
-samples = generate_tone(args.frequency, args.amplitude, args.duration, sample_rate, args.vibrato_rate, args.vibrato_depth)
-save_to_wav(samples, sample_rate, "vibrato_tone.wav")
- """
